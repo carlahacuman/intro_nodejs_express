54ef159740fdcca0169d654ff4d4cc9dead00926
unknown <YY222@ccs.local>
2025-02-05 05:27:02 -0800

    Updated index

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
+//Define a route for a get req
+app.get('/about', (req, res) => {
+    res.send('ABout Us');
+});
+
+//Middle to parse json bodies
+app.use(express.json());
+
+//Define a route for a post req
+app.post('/submit',(req, res) => {  
+    const data = req.body;
+    res.send(`REceived: ${JSON.stringify(data)}`);
+});
+
 //Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
